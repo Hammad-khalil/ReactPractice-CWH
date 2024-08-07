3f874f4ea6a63d7376284d1a4ed6ad79e3a4c589
Hammad-khalil <shhamadkhalil0786@gmail.com>
2024-08-07 12:37:41 +0500

    This is How to do Routing in ReactJS

---
+++ 115/react-router/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <div >
+    <nav >
+        <ul style={{display:"flex", gap:"20px",listStyle:"none"}}>
+            <a href="/"><li>Home</li></a>
+            <a href="/"><li>About us</li></a>
+            <a href="/"><li>Contact us</li></a>
+        </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
