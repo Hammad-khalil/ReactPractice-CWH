388dea5594605cc6c58aa9060d1e3c570c75dbd0
Hammad-khalil <shhamadkhalil0786@gmail.com>
2024-08-07 15:10:22 +0500

    Before using useContext , Props Drilling is �

---
+++ 116/useContext/src/components/Component1.jsx
@@ -0,0 +1,12 @@
+const Component1 = ({ count }) => {
+    return (
+        <>
+            <div>
+                component 1 mein agaya count finally 🥳
+            </div>
+            <h6>{count}</h6>
+        </>
+    )
+}
+
+export default Component1
---
+++ 116/useContext/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button"
+const Navbar = ({ count }) => {
+    return (
+        <>
+            <div>
+                I am Navbar
+            </div>
+            <Button count={count} />
+        </>
+    )
+}
+
+export default Navbar
